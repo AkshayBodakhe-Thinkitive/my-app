de86a8a94be3d0770e9b0fd5526b61cdbfcdcc15
Akshay Bodakhe <akshay.bodakhe@thinkitive.com>
2024-06-27 17:17:54 +0530

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ThemeContext from './context/ThemeContext';
+import ThemedButton from './components/ThemedButton';
+import { Appp } from './components/HOC';
+import Routing from './components/Routing';
+import Hooks from './hooks/Hooks';
+import UseReducer from './hooks/UseReducer';
+import ForwardRefEx from './components/ForwardRef';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {/* <Toolbar /> */}
+      {/* <Appp/> */}
+      {/* <Routing/> */}
+      {/* <Hooks/> */}
+      {/* <UseReducer/> */}
+      <ForwardRefEx/>
+    </ThemeContext.Provider>
   );
 }
 
 export default App;
+
+function Toolbar() {
+  return (
+    <div>
+      <ThemedButton />
+    </div>
+  );
+}
+
+
+
